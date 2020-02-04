70bcfc5b961fdfeee0b983fb2ce907e0fac522bd
Matej Ľach <me@matej-lach.me>
2020-02-04 21:15:09 +0000

    improve package-level doc

--- doc.go
+++ doc.go
@@ -1,6 +1,7 @@
 /*
 Package astreams implements JSON serialization/deserialization support for the ActivityStreams 2.0 vocabulary.
-Simply use json.Marshal/json.Unmarshal on AS 2.0 compliant objects to obtain their Go/JSON representation respectively.
-*/
+Simply use json.Marshal/json.Unmarshal on AS 2.0 compliant objects to obtain their JSON/Go representation respectively.
 
+Indirect object references such as a link to an outbox collection within an actor profile need to be fetched explicitly and parsed as a separate object.
+*/
 package astreams
